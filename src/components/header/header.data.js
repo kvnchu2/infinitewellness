0c745a1df2ef55c74a669ee0803749db5bf92da2
Kevin Chu <kvnchu2@gmail.com>
2022-07-03 13:49:08 -0700

    initial commit

---
+++ src/components/header/header.data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    path: 'home',
+    label: 'Home',
+  },
+  {
+    path: 'feature',
+    label: 'Features',
+  },
+  {
+    path: 'teamsection',
+    label: 'Team',
+  },
+  {
+    path: 'pricing',
+    label: 'Pricing',
+  },
+  {
+    path: 'testimonial',
+    label: 'Testimonial',
+  },
+];
