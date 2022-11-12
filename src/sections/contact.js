1897164fd6036703ef9e3ec048cf59d5c2ea6a67
Kevin Chu <kvnchu2@gmail.com>
2022-11-12 15:22:02 -0800

    add new form

--- src/sections/contact.js
+++ src/sections/contact.js
@@ -52,6 +52,27 @@ export default function Contact(props)  {
                 </p>
               </div>
               <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+
+              {/* <form name="contact" method="POST" data-netlify="true">
                 <div className='row'>
                   <div className='col-md-6'>
                     <div className='form-group'>
@@ -98,7 +119,7 @@ export default function Contact(props)  {
                 <button type='submit' className='btn btn-custom btn-lg'>
                   Send Message
                 </button>
-              </form>
+              </form> */}
             </div>
           </div>
           <div className='col-md-3 col-md-offset-1 contact-info'>
