8d19131e0667f2efaa30c5d323c3c46d1a9b3bc1
Kevin Chu <kvnchu2@gmail.com>
2022-11-12 22:08:31 -0800

    add thanks page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,7 @@
 export default function Contact(props)  {
 
   return (
-    <form name="contact" method="POST" data-netlify="true">
+    <form name="contact" action="/thanks" method="POST" data-netlify="true">
       <input type="hidden" name="form-name" value="contact" />
       <p>
         <label>Your Name: <input type="text" name="name" /></label>
